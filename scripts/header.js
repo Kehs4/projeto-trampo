94aa2b78e91061505c31369759462faf43fec394
Kleyton Holanda <69331002+Kehs4@users.noreply.github.com>
2024-08-29 09:37:58 -0300

    Add files via upload

---
+++ scripts/header.js
@@ -0,0 +1,3 @@
+let cabecalho = document.getElementsByClassName('cabecalho_dinamico')[0]
+
+cabecalho.innerHTML='<div><div class="logoHeader"><a href="index.html" draggable="false"><img class="logo" src="mainpage/img/logo.png" draggable="false"></a></div></div>'
